1bff4f72636abad1da364695042f51c0f72a16ec
Alexei Shilin <alexei.shilin@gmail.com>
2016-05-09 23:12:43 +0600

    refactor load question in controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,12 @@
 class QuestionsController < ApplicationController
 
+  before_action :load_question, only: [:show, :edit]
+
   def index
     @questions = Question.all
   end
 
   def show
-    @question = Question.find(params[:id])
   end
 
   def new
@@ -13,7 +14,6 @@ class QuestionsController < ApplicationController
   end
 
   def edit
-    @question = Question.find(params[:id])
   end
 
   def create
@@ -30,6 +30,10 @@ class QuestionsController < ApplicationController
 
   def question_params
     params.require(:question).permit(:title, :body)
+  end
+
+  def load_question
+    @question = Question.find(params[:id])
   end
 
 end
