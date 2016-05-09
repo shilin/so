20814352d114c0114ba4c64ee746b1b1e87056f5
Alexei Shilin <alexei.shilin@gmail.com>
2016-05-10 01:18:11 +0600

    add ability to answer

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  resources :questions
+  resources :questions, shallow: true do
+    resources :answers
+  end
 
   # Example resource route with options:
   #   resources :products do
