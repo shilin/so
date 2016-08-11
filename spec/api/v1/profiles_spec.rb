fd70bfd64102902e0180308e24d9e03f34ae1211
Alexei Shilin <alexei.shilin@gmail.com>
2016-08-11 23:58:26 +0700

    add api methods for getting profiles of auth user and other users

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -1,7 +1,39 @@
 require 'rails_helper'
 
 describe 'Profile API' do
-  describe 'GET /me' do
+  let(:me) { create(:user) }
+  let!(:other_users) { create_list(:user, 3) }
+  let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+
+  describe 'GET #index' do
+    context 'unauthorized' do
+      it 'returns unauthorized status if there is no access_token' do
+        get '/api/v1/profiles/', format: :json
+        expect(response).to have_http_status :unauthorized
+      end
+
+      it 'returns unauthorized status if access_token is invalid' do
+        get '/api/v1/profiles/', format: :json, access_token: '1234'
+        expect(response).to have_http_status :unauthorized
+      end
+    end
+
+    context 'authorized' do
+      before { get '/api/v1/profiles/', format: :json, access_token: access_token.token }
+
+      it 'returns OK status' do
+        expect(response).to have_http_status :ok
+      end
+
+      it 'contains json with the other users' do
+        other_users.each do |user|
+          expect(response.body).to include_json(user.to_json)
+        end
+      end
+    end
+  end
+
+  describe 'GET #me' do
     context 'unauthorized' do
       it 'returns unauthorized status if there is no access_token' do
         get '/api/v1/profiles/me', format: :json
@@ -15,9 +47,6 @@ describe 'Profile API' do
     end
 
     context 'authorized' do
-      let(:me) { create(:user) }
-      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
-
       before { get '/api/v1/profiles/me', format: :json, access_token: access_token.token }
 
       it 'returns OK status' do
