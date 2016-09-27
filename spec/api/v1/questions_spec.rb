5c1a4e104873c95cf0b504bfa01e04bf603ec088
Alexei Shilin <alexei.shilin@gmail.com>
2016-09-28 00:37:17 +0700

    remove duplicated testing from api index

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 describe 'Questions API' do
@@ -7,8 +8,6 @@ describe 'Questions API' do
     context 'authorized' do
       let(:access_token) { create(:access_token) }
       let!(:questions) { create_list(:question, 2) }
-      let(:question) { questions.first }
-      let!(:answer) { create(:answer, question: question) }
 
       before { get api_v1_questions_path, format: :json, access_token: access_token.token }
 
@@ -18,32 +17,6 @@ describe 'Questions API' do
 
       it 'returns list of questions' do
         expect(response.body).to have_json_size(2).at_path('questions')
-      end
-
-      %w(id title body created_at updated_at).each do |attr|
-        it "question object contains #{attr}" do
-          expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("questions/0/#{attr}")
-        end
-      end
-
-      it 'question object contains short_title' do
-        expect(response.body).to be_json_eql(question.title.truncate(10).to_json).at_path('questions/0/short_title')
-      end
-
-      context 'answers' do
-        it 'is included in parent question' do
-          expect(response.body).to have_json_size(1).at_path('questions/0/answers')
-        end
-
-        %w(id body created_at updated_at).each do |attr|
-          it "contains #{attr}" do
-            expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("questions/0/answers/0/#{attr}")
-          end
-        end
-
-        it 'contains exactly required attributes' do
-          expect(JSON.parse(response.body)['questions'][0]['answers'][0].keys).to contain_exactly('id', 'question_id', 'body', 'created_at', 'updated_at')
-        end
       end
     end
 
